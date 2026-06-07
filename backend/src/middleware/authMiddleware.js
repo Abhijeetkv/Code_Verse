ea6e8ae72e2459920ef55aaf328efdab6978f215
Abhijeetkv <abhijeetkv2@gmail.com>
2026-06-07 11:53:33 +0530

    refactor: enhance error handling and logging in protectedRoute middleware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -2,25 +2,42 @@ import { requireAuth } from "@clerk/express";
 import User from "../modals/User.js";
 
 export const protectedRoute = [
-    requireAuth(),
-    async (req, res, next) => {
-        try {
-            const clerkId = req.auth().userId;
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      console.log("Auth object:", req.auth);
 
-            if(!clerkId) {
-                return res.status(401).json({message: "Unauthorized"});
-            }
+      const clerkId = req.auth?.userId;
 
-            const user = await User.findOne({clerkId});
+      if (!clerkId) {
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized - No Clerk User ID",
+        });
+      }
 
-            if(!user) {
-                return res.status(404).json({message: "User not found"});
-            }
-            req.user = user;
-            next();
-        } catch (error) {
-            console.error("Error in protected route middleware:", error);
-            return res.status(500).json({message: "Internal Server Error"});
-        }
+      console.log("Clerk ID:", clerkId);
+
+      const user = await User.findOne({ clerkId });
+
+      if (!user) {
+        console.log("User not found in DB for Clerk ID:", clerkId);
+
+        return res.status(404).json({
+          success: false,
+          message: "User not found",
+        });
+      }
+
+      req.user = user;
+      next();
+    } catch (error) {
+      console.error("Protected Route Error:", error);
+
+      return res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+      });
     }
-]
+  },
+];
